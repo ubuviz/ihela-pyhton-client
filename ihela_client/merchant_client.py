67912f280992fffca6e79f9a1519efc5289462ae
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2023-05-29 11:18:51 +0200

    Separated example from the merchant client file|

--- ihela_client/merchant_client.py
+++ ihela_client/merchant_client.py
@@ -10,18 +10,8 @@ Python client for integration
 import json
 import logging
 
-# import string
-import simplejson
-
-# import urllib.parse
-
-
-try:
-    import secrets
-except ImportError:  # Python < 3.6
-    import random as secrets
-
 import requests
+import simplejson
 
 logger = logging.getLogger(__name__)
 
@@ -36,7 +26,7 @@ iHela_ENDPOINTS = {
     "PING": "api/v2/ping/",
     "USER_INFO": "api/v2/connected-user/",
     "BILL_INIT": "api/v2/payments/bill/init/",
-    "BILL_VERIFY": "api/v2/payments/bill/verify/",
+    "OPERATION_STATUS": "api/v2/operations/status/",
     "CASHIN": "api/v2/payments/cash-in/",
     # "BANKS": "api/v2/bank/all",
     "BANKS": "api/v2/payments/bank/",
@@ -83,14 +73,21 @@ class MerchantClient:
     def get_response(self, resp):
         try:
             resp_json = dict(resp.json())
-            resp_json["response_status"] = resp.status_code
+            # resp_json["response_status"] = resp.status_code
             logger.debug(resp_json)
             return resp_json
         except (json.decoder.JSONDecodeError, simplejson.errors.JSONDecodeError):
             logger.error("IHELA_CLIENT_ERROR : %s" % resp.text)
             return {
-                "errors": {"request": "An error occured during request"},
-                "bill": {},
+                "success": False,
+                "response_code": "01",
+                "response_data": {
+                    "errors": {
+                        "request": "An error occured during request",
+                        "status": resp.status_code,
+                    }
+                },
+                "response_message": "An error occured during request",
             }
 
     def get_url(self, url):
@@ -192,16 +189,6 @@ class MerchantClient:
         else:
             return self.no_auth_response
 
-    def verify_bill(self, code, reference):
-        bill_data = {"code": code, "reference": reference}
-        url = iHela_ENDPOINTS["BILL_VERIFY"]
-        bill_ = requests.post(
-            self.get_url(url), data=bill_data, headers=self.get_auth_headers()
-        )
-        bill_verified = self.get_response(bill_)
-
-        return bill_verified
-
     def cashin_client(
         self,
         bank_slug,
@@ -231,6 +218,19 @@ class MerchantClient:
         else:
             return self.no_auth_response
 
+    def operations_status(self, external_reference, ihela_reference):
+        status_data = {
+            "external_reference": external_reference,
+            "reference": ihela_reference,
+        }
+        url = iHela_ENDPOINTS["OPERATION_STATUS"]
+        status_ = requests.post(
+            self.get_url(url), data=status_data, headers=self.get_auth_headers()
+        )
+        ope_status = self.get_response(status_)
+
+        return ope_status
+
     def get_bank_list(self, list_type=None):
         if not list_type:
             list_type = "cashin"
@@ -242,58 +242,3 @@ class MerchantClient:
             return banks
         else:
             return self.no_auth_response
-
-
-if __name__ == "__main__":
-    client_id = "4sS7OWlf8pqm04j1ZDtvUrEVSZjlLwtfGUMs2XWZ"
-    client_id = "12CMmsS2e3aqONxYHCSpHaG3p7VWls9vtczbNk1b"
-    # client_id = "KziHxNoydAhWV2uVSfimZf7ApMY1tdjW9vYXfGwk"
-    client_secret = "HN7osYwSJuEOO4MEth6iNlBS8oHm7LBhC8fejkZkqDJUrvVQodKtO55bMr845kmplSlfK3nxFcEk2ryiXzs1UW1YfVP5Ed6Yw0RR6QmnwsQ7iNJfzTgeehZ2XM9mmhC3"  # noqa
-    client_secret = "duwbLBiKPoJTytFnMcAbP8QxmaAJPboNQHslRpqgCsSplNo5Es4tBFDJl2Iae0WAErpP4QcQ0iUGpxkoFdFXnOeGDMvtX5JLVyrlvRE6DfScBagKExHdmugWwDstFHgP"  # noqa
-    # client_secret = "LjATwjOk70mGVdkyGZNxRih0FLe4lfF2UEgHAGAF7ovK38jQQ9dBdd1SSmWoXZl44wee0bFamQQclq1sQFUBL6XBsGqjRV8DR8isa2GEVNNMroLWiB1K5ZZf3H9UoCyt" # noqa
-    cl = MerchantClient(
-        client_id,
-        client_secret,
-        pin_code="1234",
-        ihela_url="http://10.30.0.7/",
-    )
-    # )  # , ihela_url="http://127.0.0.1:8080/")
-    print("\nPING IHELA : ", cl.ihela_base_url)
-    ping = cl.ping()
-    print("PING DATA : ", ping)
-
-    # bill = cl.init_bill(
-    #     2000,
-    #     # "76077736",
-    #     "pierreclaverkoko@gmail.com",
-    #     "My description",
-    #     str(secrets.token_hex(10)),
-    #     # bank="MOB-0003"
-    # )
-    # print(bill)
-
-    # if bill["bill"].get("merchant_reference"):
-    #     bill_verif = cl.verify_bill(
-    #         bill["bill"]["merchant_reference"], bill["bill"]["code"]
-    #     )
-
-    #     print("\nBILL VERIFY : ", bill_verif)
-
-    banks = cl.get_bank_list()
-
-    print("\nBANKS LIST : ", banks)
-
-    client = cl.customer_lookup("MF1-0001", "000016-01")
-
-    print("\nCUSTOMER LOOKUP : ", client)
-
-    cashin = cl.cashin_client(
-        "MF1-0001",
-        "000016-01",
-        "Pierre Claver Koko",
-        20000,
-        str(secrets.token_hex(10)),
-        "Cashin description",
-    )
-
-    print("\nCASHIN TO CLIENT : ", cashin)
